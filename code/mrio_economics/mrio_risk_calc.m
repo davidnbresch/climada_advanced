343d0ab743bd53a3cce9ecce6b62b30fcfa6f639
KasparTo <toblekas@student.ethz.ch>
2018-01-05 16:54:35 +0100

    Added some notes/questions/suggestions and one minor change as documented.

--- code/mrio_economics/mrio_risk_calc.m
+++ code/mrio_economics/mrio_risk_calc.m
@@ -1,4 +1,4 @@
-function [country_risk,leontief_inverse]=mrio_risk_calc(climada_mrio, country_risk_direct) % uncomment to run as function
+function [country_risk,leontief_inverse]=mrio_risk_calc(climada_mriot, country_risk_direct) % uncomment to run as function
 % mrio entity
 % MODULE:
 %   advanced
@@ -14,11 +14,11 @@ function [country_risk,leontief_inverse]=mrio_risk_calc(climada_mrio, country_ri
 %   next call:  % just to illustrate
 %
 % CALLING SEQUENCE:
-%   [country_risk]=mrio_risk_calc(climada_mrio, country_risk_direct)
+%   [country_risk]=mrio_risk_calc(climada_mriot, country_risk_direct)
 % EXAMPLE:
 %   climada_read_mriot;
 %   mrio_master('Switzerland','Agriculture','100y');
-%   [country_risk] = mrio_risk_calc(climada_mrio, country_risk_direct);
+%   [country_risk] = mrio_risk_calc(climada_mriot, country_risk_direct);
 % INPUTS:
 %   climada_mriot: a structure with ten fields. It represents a general climada
 %   mriot structure whose basic properties are the same regardless of the
@@ -31,7 +31,10 @@ function [country_risk,leontief_inverse]=mrio_risk_calc(climada_mrio, country_ri
 %   leontief_inverse: the leontief inverse matrix which relates final demand to production 
 % MODIFICATION HISTORY:
 % Ediz Herms, ediz.herms@outlook.com, 20171207, initial
-%-
+% Kaspar Tobler, 20180105, added a few notes/questions. See "Note KT".
+% Kaspar Tobler, 20180105, changed order of matrix multiplication in 
+    % calculation of country_risk. 
+
 
 country_risk = []; % init output
 leontief_inverse = []; % init output
@@ -47,9 +50,24 @@ if ~climada_init_vars,return;end % init/import global variables
 
 % Calculate technical coefficient matrix 
 [m,n] = size(climada_mriot.mrio_data);
-total_output = sum(climada_mriot.mrio_data); % total output per sector per country (sum up columns)
+% total_output = sum(climada_mriot.mrio_data); % total output per sector per country (sum up columns)
+                % Note KT: Above we have to sum up the individual rows, not columns,
+                % I think. The column sum would be total input per sector/country from all
+                % other sectors:
+     total_output = sum(climada_mriot.mrio_data,2)'; % Transpose to get row vector.
 for sector=1:1:m
     leontief_inverse(:,sector) = climada_mriot.mrio_data(:,sector)/total_output(sector); % normalize with total output
 end
 
-country_risk = inv(eye(size(climada_mriot.mrio_data)) - leontief_inverse) * country_risk_direct;
+% Note KT: Maybe calculate Leontief inverse based on technical coefficients first
+% and then in separate step final calculation of indirect risk. For
+% clarity (also, we might want to offer technical_coeffs matrix as an optional output):
+    % First L = (I-A)^-1:
+    % technical_coeffs = leontief_inverse;
+    % leontief_inverse = inv(eye(size(climada_mriot.mrio_data)) - technical_coeffs);
+    % Then final step indir. risk = dirc. risk * L:
+    % country_risk = country_risk_direct * leontief_inverse;
+    
+% country_risk = inv(eye(size(climada_mriot.mrio_data)) - leontief_inverse) * country_risk_direct;
+% Change order of matrix multiplication:
+    country_risk =  country_risk_direct * inv(eye(size(climada_mriot.mrio_data)) - leontief_inverse);
