7993e58deb3e1da0cc138ddf36c85f0d3bbfc91d
EdizH <33393551+EdizH@users.noreply.github.com>
2018-01-12 14:12:41 +0100

    calculation based on mrio country list
    Introduce a normalization of asset list based on mrio country list. This indicates also a mapping of all countries which are not explicitely mentioned to 'RoW'.

--- code/mrio_economics/mrio_entity.m
+++ code/mrio_economics/mrio_entity.m
@@ -1,4 +1,4 @@
-function [entity,hazard]=mrio_entity(params) % uncomment to run as function
+function [entity,hazard] = mrio_entity(climada_mriot, params) % uncomment to run as function
 % mrio entity
 % MODULE:
 %   advanced
@@ -10,14 +10,18 @@ function [entity,hazard]=mrio_entity(params) % uncomment to run as function
 %   NOTE: see PARAMETERS in code
 %
 %   previous call: see isimip_gdp_entity to generate the global centroids and entity
-%   next call: EDS=climada_EDS_calc(entity,hazard); % just to illustrate
+%   next call: EDS = climada_EDS_calc(entity,hazard); % just to illustrate
 % CALLING SEQUENCE:
-%   [entity,hazard]=mrio_entity(params)
+%   [entity,hazard] = mrio_entity(climada_mriot, params)
 % EXAMPLE:
-%   [entity,hazard]=mrio_entity;
-%   params.plot_centroids=1;params.plot_entity=1;
-%   [entity,hazard]=mrio_entity(params)
+%   climada_mriot = climada_read_mriot;
+%   params.plot_centroids = 1; params.plot_entity = 1;
+%   [entity,hazard] = mrio_entity(climada_mriot, params)
 % INPUTS:
+%   climada_mriot: a struct with ten fields, one of them being countries_iso.
+%   The latter is important for this function. The struct represents a general climada
+%   mriot structure whose basic properties are the same regardless of the
+%   provided mriot it is based on, see climada_read_mriot; 
 % OPTIONAL INPUT PARAMETERS:
 %   params: a structure with the fields
 %       plot_centroids: =1 to plot the centroids, =0 not (default)
@@ -28,10 +32,15 @@ function [entity,hazard]=mrio_entity(params) % uncomment to run as function
 % MODIFICATION HISTORY:
 % David N. Bresch, david.bresch@gmail.com, 20171206, initial
 % Ediz Herms, ediz.herms@outlook.com, 20171207, normalize assets per country
+% Ediz Herms, ediz.herms@outlook.com, 20180112, ...mrio table as input
 %-
 
-entity=[]; % init output
-hazard=[]; % init output
+params = struct;
+params.plot_centroids = 1;
+params.plot_entity=1; 
+
+entity = []; % init output
+hazard = []; % init output
 
 global climada_global
 if ~climada_init_vars,return;end % init/import global variables
@@ -40,7 +49,7 @@ if ~climada_init_vars,return;end % init/import global variables
 
 % poor man's version to check arguments
 % and to set default value where  appropriate
-if ~exist('params','var'),params=struct;end % in case we want to pass all parameters as structure
+if ~exist('params','var'), params = struct; end % in case we want to pass all parameters as structure
 
 % locate the module's (or this code's) data folder (usually  a folder
 % 'parallel' to the code folder, i.e. in the same level as code folder)
@@ -51,31 +60,31 @@ if ~exist('params','var'),params=struct;end % in case we want to pass all parame
 % define all parameters here - no parameters to be defined in code below
 %
 % the global centroids
-centroids_file='GLB_NatID_grid_0360as_adv_1';
+centroids_file = 'GLB_NatID_grid_0360as_adv_1';
 %
 % the global entity
-entity_file='GLB_0360as_ismip_2018';
+entity_file = 'GLB_0360as_ismip_2018';
 
 % the (TEST) hazard
-hazard_file='GLB_0360as_TC_hist'; % historic
+hazard_file = 'GLB_0360as_TC_hist'; % historic
 %hazard_file='GLB_0360as_TC'; % probabilistic, 10x more events than hist
 
 
 % setup structure to pass all parameters:
 if isstruct(params)
-    if ~isfield(params,'plot_centroids'),params.plot_centroids=[];end
-    if ~isfield(params,'plot_entity'),params.plot_entity=[];end
+    if ~isfield(params,'plot_centroids'), params.plot_centroids = []; end
+    if ~isfield(params,'plot_entity'), params.plot_entity = []; end
 end
 
 % load global centroids
 fprintf('loading centroids %s\n',centroids_file);
-centroids=climada_centroids_load(centroids_file);
+centroids = climada_centroids_load(centroids_file);
 
 if params.plot_centroids % plot the centroids
     figure('Name','centroids');
-    country_pos=(centroids.centroid_ID<3e6); % find high(er) resolution centroids within countries
+    country_pos = (centroids.centroid_ID<3e6); % find high(er) resolution centroids within countries
     plot(centroids.lon(country_pos),centroids.lat(country_pos),'.g');hold on;
-    grid_pos=(centroids.centroid_ID>=3e6); % find coarse resolution centroids for regular grid
+    grid_pos = (centroids.centroid_ID>=3e6); % find coarse resolution centroids for regular grid
     plot(centroids.lon(grid_pos),centroids.lat(grid_pos),'.r','MarkerSize',.1)
     climada_plot_world_borders
     legend({'country centroids [10km]','grid centroids [100km]'})
@@ -84,14 +93,28 @@ end % params.plot_centroids
 
 % load global entity
 fprintf('loading entity %s\n',entity_file);
-entity=climada_entity_load(entity_file);
+entity = climada_entity_load(entity_file);
 
-% normalization of asset values for all countries
-for country=unique(entity.assets.NatID)
-    sel_pos = intersect(find(entity.assets.NatID==country),find(~isnan(entity.assets.Value))); % select all non-NaN assets
+country_ISO3 = entity.assets.NatID_RegID.ISO3;
+mrio_country_ISO3 = unique(climada_mriot.countries_iso);
+
+% normalization of asset values for all countries as specified in mrio table
+for i = 1:length(mrio_country_ISO3)
+    
+    country = mrio_country_ISO3(i); % extract ISO code
+    
+    if country ~= 'ROW'
+        sel_country_pos = find(ismember(country_ISO3, country)); 
+        sel_pos = intersect(find(ismember(entity.assets.NatID,sel_country_pos)),find(~isnan(entity.assets.Value))); % select all non-NaN assets % select all non-NaN assets of this country
+    else % 'Rest of World' (RoW) is viewed as a country 
+        list_RoW_ISO3 = setdiff(country_ISO3,mrio_country_ISO3); % find all countries that are not individually listed in the MRIO table 
+        list_RoW_NatID = find(ismember(country_ISO3,list_RoW_ISO3)); % extract NatID
+        sel_pos = intersect(find(ismember(entity.assets.NatID,list_RoW_NatID)),find(~isnan(entity.assets.Value))); % select all non-NaN RoW assets
+    end
+    
     entity.assets.Value(sel_pos) = entity.assets.Value(sel_pos)/sum(entity.assets.Value(sel_pos)); % normalize assets
 end
-        
+
 if params.plot_entity % plot the centroids
     figure('Name','entity');
     climada_entity_plot(entity);
@@ -99,6 +122,6 @@ end % params.plot_entity
 
 % load tropical cyclone hazard set
 fprintf('loading hazard %s\n',hazard_file);
-hazard=climada_hazard_load(hazard_file);
+hazard = climada_hazard_load(hazard_file);
 
 end % mrio_entity
--- code/mrio_economics/mrio_master.m
+++ code/mrio_economics/mrio_master.m
@@ -9,12 +9,12 @@
 % CALLING SEQUENCE:
 %   mrio_master(country_name, sector_name)
 % EXAMPLE:
-%   mrio_master('Switzerland','Agriculture','100y')
+%   mrio_master('Switzerland','Agriculture','EAD')
 % INPUTS:
 %   country_name: name of country (string)
 %   sector_name: name of sector (string)
 % OPTIONAL INPUT PARAMETERS:
-%   risk_measure: risk measure to be applied (string), default is the Annual Average Loss
+%   risk_measure: risk measure to be applied (string), default is the Expected Annual Damage (EAD)
 % OUTPUTS:
 %
 % MODIFICATION HISTORY:
@@ -24,54 +24,78 @@
 %global climada_global
 
 % read MRIO table
-climada_mriot = limada_read_mriot;
+climada_mriot = climada_read_mriot;
 
-% proceed with aggregated numbers / rough sector classification
+% proceed with aggregated numbers
 
-% for sector = 1:climada_mriot(1).no_of_sectors
+risk_direct = zeros(0,climada_mriot.no_of_sectors*climada_mriot.no_of_countries);
+country_risk_direct = zeros(0,climada_mriot.no_of_countries);
 
-% load centroids and prepare entities for mrio
-[entity,hazard]=mrio_entity;
+% load centroids and prepare entities for mrio risk estimation 
+[entity,hazard] = mrio_entity(climada_mriot);
 
-% calculation for all countries
-for country=unique(entity.assets.NatID)
-    sel_pos = ismember(entity.assets.NatID,23);
-    entity_sel = entity;
-    entity_sel.assets.Value = entity.assets.Value .* sel_pos;  % set values = 0 for all assets outside country i.
+country_ISO3 = entity.assets.NatID_RegID.ISO3; % above list is too fine-grided for our purpose (e.g. treats Hawaii as own country)
+mrio_country_ISO3 = unique(climada_mriot.countries_iso);
 
-    % calculate event damage set
+% calculation for all countries as specified in mrio table
+for i = 1:length(mrio_country_ISO3)
     
-    EDS = climada_EDS_calc(entity_sel,hazard,'','',2,'');
+    % for sector = 1:climada_mriot.no_of_sectors (here to have same structure as in mrio)
+    
+    country = mrio_country_ISO3(i); % extract ISO code
 
+    if country ~= 'ROW' 
+        sel_country_pos = find(ismember(country_ISO3, country)); 
+        sel_assets = eq(ismember(entity.assets.NatID,sel_country_pos),~isnan(entity.assets.Value)); % select all non-NaN assets of this country
+    else % 'Rest of World' (RoW) is viewed as a country 
+        list_RoW_ISO3 = setdiff(country_ISO3,mrio_country_ISO3); % find all countries that are not individually listed in the MRIO table 
+        list_RoW_NatID = find(ismember(country_ISO3,list_RoW_ISO3)); % extract NatID
+        sel_assets = eq(ismember(entity.assets.NatID,list_RoW_NatID),~isnan(entity.assets.Value)); % select all non-NaN RoW assets
+    end
+    
+    entity_sel = entity;
+    entity_sel.assets.Value = entity.assets.Value .* sel_assets;  % set values = 0 for all assets outside country i.
+    
+    % calculate event damage set
+    EDS = climada_EDS_calc(entity_sel,hazard,'','',2,'');
+    
     % Calculate Damage exceedence Frequency Curve (DFC)
     DFC = climada_EDS_DFC(EDS);
     
     % convert an event (per occurrence) damage set (EDS) into a year damage set (YDS)
     YDS = climada_EDS2YDS(EDS,hazard);
     
+    % quantify risk with specified risk measure 
     switch risk_measure
-        case 'Annual Average Loss' % expected damage 
-            country_risk_direct(country) = YDS.ED;
-        case '100y-event' % 
+        case 'EAD' % Expected Annual Damage
+            risk_direct(i) = YDS.ED;
+        case '100y-event' %
             return_period = 100;
             sort_damages = sort(YDS.damage);
             sel_pos = max(find(DFC.return_period >= return_period));
-            country_risk_direct(country) = DFC.damage(sel_pos);
-        case '50y-event' % 
+            risk_direct(i) = DFC.damage(sel_pos);
+        case '50y-event' %
             return_period = 50;
             sort_damages = sort(YDS.damage);
             sel_pos = max(find(DFC.return_period >= return_period));
-            country_risk_direct(country) = DFC.damage(sel_pos);
-        case '20y-event' % 
+            risk_direct(i) = DFC.damage(sel_pos);
+        case '20y-event' %
             return_period = 20;
             sort_damages = sort(YDS.damage);
             sel_pos = max(find(DFC.return_period >= return_period));
-            country_risk_direct(country) = DFC.damage(sel_pos);
-        case 'worst-case' % 
+            risk_direct(i) = DFC.damage(sel_pos);
+        case 'worst-case' %
+            sel_pos = max(find(DFC.return_period));
+            risk_direct(i) = DFC.damage(sel_pos);
         otherwise
+            % ask user to choose out of a list and return to beginning of switch statement
     end
+    %end
 end
 
-%end
+% disaggregate direct risk to all subsectors for each country
 
-% quantifying indirect risk using the Leontief I-O model
+%country_risk_direct = cumsum(risk_direct);
+
+% estimation of indirect risk using Leontief I/O model 
+[risk] = mrio_leontief_calc(climada_mriot, risk_direct)
