60ed5537e66093bf11148d32726c3324fd2d96bc
Ediz Herms <33393551+EdizH@users.noreply.github.com>
2018-01-31 10:34:29 +0100

    new input: params; eliminate redundancies
    Especially for development phase: obtain a paramaters struct which contains default values for all key parameters which are passed as function arguments during the entire standard process following the mrio_master file. Saves time.

--- code/mrio_economics/mrio_get_params.m
+++ code/mrio_economics/mrio_get_params.m
@@ -1,9 +1,9 @@
 function params = mrio_get_params
-% mrio master
+% mrio get params
 % MODULE:
 %   advanced
 % NAME:
-%   mrio_master
+%   mrio_get_params
 % PURPOSE:
 %   Especially for development phase: obtain a paramaters struct which contains
 %   default values for all key parameters which are passed as function
@@ -18,9 +18,9 @@ function params = mrio_get_params
 %   None.
 % OUTPUTS:
 %   params: a struct containing several fields, some of which are struct
-%   themselves that contain default values of all relevant function
-%   parameters used in the mrio_master file. Saves time working through the
-%   master file (i.e. the whole standard computation process), especially during development phase.
+%       themselves that contain default values of all relevant function
+%       parameters used in the mrio_master file. Saves time working through the
+%       master file (i.e. the whole standard computation process), especially during development phase.
 % MODIFICATION HISTORY:
 % Kaspar Tobler, 20180119, initialized and finished first working version.
 
@@ -28,14 +28,19 @@ global climada_global
 if ~climada_init_vars,return;end % init/import global variables
 
 climada_global.waitbar = 0;
+
 params.mriot.file_name = 'WIOT2014_Nov16_ROW.xlsx';
 params.mriot.table_flag = 'wiod';
 params.centroids_file = 'GLB_NatID_grid_0360as_adv_1'; % the global centroids
-params.entity_file.agri = 'GLB_0360as_ismip_2018'; % the global Agriculture (agri) entity
-params.entity_file.for = 'GLB_0360as_ismip_2018'; % the global Forestry and Fishing (for) entity
-params.entity_file.min = 'GLB_0360as_ismip_2018'; % the global Mining and Quarrying (min) entity
-params.entity_file.manu = 'GLB_0360as_ismip_2018'; % the global Manufacturing (manu) entity
-params.entity_file.serv = 'GLB_0360as_ismip_2018'; % the global Services (serv) entity
-params.entity_file.utilities = 'GLB_0360as_ismip_2018'; % the global Electricity, Gas and Water supply (utilities) entity
 params.hazard_file = 'GLB_0360as_TC_hist'; % historic
 % params.hazard_file='GLB_0360as_TC'; % probabilistic, 10x more events than hist
+
+params.entity_file.agriculture = 'GLB_agriculture_ismip_2018'; % the global Agriculture (agriculture) entity
+params.entity_file.forestry_fishing = 'GLB_0360as_ismip_2018_prep'; % the global Forestry and Fishing (forestry_fishing) entity
+params.entity_file.mining_quarrying = 'GLB_mining_quarrying_ismip_2018'; % the global Mining and Quarrying (mining_quarrying) entity
+params.entity_file.manufacturing = 'GLB_0360as_ismip_2018_prep'; % the global Manufacturing (manufacturing) entity
+params.entity_file.services = 'GLB_0360as_ismip_2018_prep'; % the global Services (services) entity
+params.entity_file.utilities = 'GLB_0360as_ismip_2018_prep'; % the global Electricity, Gas and Water supply (utilities) entity
+params.entity_file = struct2cell(params.entity_file);
+
+end % mrio_get_params
--- code/mrio_economics/mrio_master.m
+++ code/mrio_economics/mrio_master.m
@@ -1,4 +1,4 @@
-% function [subsector_risk, country_risk, leontief_inverse, climada_nan_mriot] = mrio_master(admin0_name, subsector_name, risk_measure) % uncomment to run as function
+% function [subsector_risk, country_risk, leontief_inverse, climada_nan_mriot] = mrio_master(country_name, subsector_name, risk_measure) % uncomment to run as function
 % mrio master
 % MODULE:
 %   advanced
@@ -7,19 +7,34 @@
 % PURPOSE:
 %   master script to run mrio calculation (multi regional I/O table project)
 % CALLING SEQUENCE:
-%   mrio_master(country_name, sector_name, risk_measure)
+%   mrio_master(country_name, subsector_name, risk_measure)
 % EXAMPLE:
-%   mrio_master('Switzerland', 'Agriculture', 'EAD')
+%   mrio_master('Switzerland', 'Manufacture of chemicals and chemical products', 'EAD')
 %   mrio_master
 % INPUTS:
-%  
+%
 % OPTIONAL INPUT PARAMETERS:
-%   admin0_name: the country name, either full (like 'Puerto Rico')
+%   country_name: the country name, either full (like 'Puerto Rico')
 %       or ISO3 (like 'PRI'). See climada_country_name for names/ISO3
 %   subsector_name: the subsector name, see e.g. mrio_read_table
 %   risk_measure: the risk measure to be applied (string), default is ='EAD' (Expected Annual Damage)
 % OUTPUTS:
-%   risk: risk per country and sector based on the risk measure chosen
+%   direct_subsector_risk: a table containing as one variable the direct risk for each
+%       subsector/country combination covered in the original mriot. The
+%       order of entries follows the same as in the entire process, i.e.
+%       entry mapping is still possible via the climada_mriot.setors and
+%       climada_mriot.countries arrays. The table further contins three
+%       more variables with the country names, country ISO codes and sector names
+%       corresponging to the direct risk values.
+%  direct_country_risk: a table containing as one variable the direct risk per country (aggregated across all subsectors) 
+%       based on the risk measure chosen. Further a variable with correpsonding country
+%       names and country ISO codes, respectively.
+%   subsector_risk: table with indirect risk per subsector/country combination 
+%       based on the risk measure chosen in one variable and three "label" variables 
+%       containing corresponding country names, country ISO codes and sector names.
+%   country_risk: table with indirect risk per country based on the risk measure chosen
+%       in one variable and two "label" variables containing corresponding 
+%       country names and country ISO codes.
 %   leontief_inverse: the leontief inverse matrix which relates final demand to production
 %   climada_nan_mriot: matrix with the value 1 in relations (trade flows) that cannot be accessed
 % MODIFICATION HISTORY:
@@ -31,7 +46,7 @@
          
 % poor man's version to check arguments
 if ~exist('risk_measure', 'var'), risk_measure = []; end
-%if ~exist('admin0_name', 'var'), country_name = []; end
+%if ~exist('country_name', 'var'), country_name = []; end
 %if ~exist('subsector_name', 'var'), sector_name = []; end
 %if ~exist('silent_mode','var'), silent_mode = 0; end
 
@@ -48,17 +63,9 @@ climada_mriot = mrio_read_table(params.mriot.file_name,params.mriot.table_flag);
 fprintf('Aggregating MRIO table...\n');tic;
 aggregated_mriot = mrio_aggregate_table(climada_mriot);toc
 
-% load (TEST) hazard
-fprintf('Loading hazard set...\n');tic;
-hazard = climada_hazard_load(params.hazard_file);toc
-
-% load centroids and prepare entities for mrio risk estimation 
-fprintf('Loading centroids and prepare entities for mrio risk estimation...\n');tic;
-entity = mrio_entity_prep(params.entity_file.agri, params.centroids_file, climada_mriot);toc
-
 % calculate direct risk for all countries and sectors as specified in mrio table
 fprintf('Calculating direct risk for all countries and sectors as specified in mrio table...\n');tic;
-[direct_subsector_risk, direct_country_risk] = mrio_direct_risk_calc(entity, hazard, climada_mriot, aggregated_mriot, risk_measure);toc
+[direct_subsector_risk, direct_country_risk] = mrio_direct_risk_calc(params, climada_mriot, aggregated_mriot, risk_measure);toc
 
 % finally, quantifying indirect risk using the Leontief I-O model
 fprintf('Quantifying indirect risk using the Leontief I-O model...\n');tic;
