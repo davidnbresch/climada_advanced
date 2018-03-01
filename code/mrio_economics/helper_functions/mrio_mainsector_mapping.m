4c5291ec77996c81b107a0f257f02ece64ddac6f
KasparTo <toblekas@student.ethz.ch>
2018-03-01 14:24:22 +0100

    Mainsector name to index mapping function, currently following principle of alphabetical order.

---
+++ code/mrio_economics/helper_functions/mrio_mainsector_mapping.m
@@ -0,0 +1,72 @@
+function mainsector_ids = mrio_mainsector_mapping(mainsector_names)
+
+% MODULE:
+%   climada_advanced
+% NAME:
+%   mrio_mainsector_mapping
+% PURPOSE:
+%   Helper function. Used e.g. by mrio_read_table.
+%   Maps an input array of mainsector names to mainsector IDs.
+%   
+%   previous call:
+% 
+% CALLING SEQUENCE:
+%  
+% EXAMPLE:
+%   climada_sect_id = mrio_mainsector_mapping(climada_sect_name) 
+%
+% INPUTS:
+%   A cell or categorical array containing mainsector names.
+%
+% OPTIONAL INPUT PARAMETERS:
+%
+% OUTPUTS:
+%   mainsector_ids: 
+%       an array containing mainsector IDs, where each array element
+%       corresponds to the element at the same position in the provided array of names.
+%   
+% GENERAL NOTES:
+%
+% POSSIBLE EXTENSIONS TO BE IMPLEMENTED:
+%
+% MODIFICATION HISTORY:
+% Kaspar Tobler, 20180216 initializing function
+
+mainsector_ids=[]; % init output
+
+global climada_global
+if ~climada_init_vars,return;end % init/import global variables
+
+% poor man's version to check arguments
+% and to set default value where  appropriate
+if ~exist('mainsector_names','var')
+    error('No input argument provided. Function requires an input cell or categorical array containing sector names.')
+end 
+
+% locate the module's data folder (here  one folder
+% below of the current folder, i.e. in the same level as code folder)
+% account for different directory structures
+if exist([climada_global.modules_dir filesep 'advanced' filesep 'data'],'dir') 
+    module_data_dir=[climada_global.modules_dir filesep 'advanced' filesep 'data'];
+else
+    module_data_dir=[climada_global.modules_dir filesep 'climada_advanced' filesep 'data'];
+end
+
+% PARAMETERS
+% First create mapping table based on alphabetically ordered unique names
+% which is then used in the second part to map an ID to the full provided
+% array...
+unique_names = unique(mainsector_names)'; % Not 'stable', hence automatically alphabetically sorted.
+mapping_table = table(unique_names,[1:6]','VariableNames',{'name','ID'});    %#ok 
+
+% Perform actual mapping:
+mainsector_ids = zeros(1,length(mainsector_names));
+for name_i = 1:length(mainsector_names)
+    mainsector_ids(name_i) = mapping_table.ID(mapping_table.name == mainsector_names(name_i));
+end
+
+
+
+
+
+
