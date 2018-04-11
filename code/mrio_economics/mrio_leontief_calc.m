8874fc402b06c4d8ba9244c087b300c83776f3b9
Ediz Herms <33393551+EdizH@users.noreply.github.com>
2018-04-11 22:25:53 +0200

    option to choose between IIM and EEIOA methodology
    Possibility to choose between the Inoperability Input-Output Model (IIM), see Anderson et al (2007) [1] and the Environmentally Extended Input-Output Analysis (EEIOA), see Kitzes (2013) [2]. By default IIM methodology is applied to estimate indirect risk.

--- code/mrio_economics/mrio_leontief_calc.m
+++ code/mrio_economics/mrio_leontief_calc.m
@@ -5,7 +5,18 @@ function [total_subsector_risk, total_country_risk, indirect_subsector_risk, ind
 % NAME:
 %   mrio_leontief_calc
 % PURPOSE:
-%   Derive total risk whereby we are using Leontief I/O model to estimate indirect risk
+%   Derive total risk whereby we are using Leontief I/O models to estimate
+%   indirect risk. There are two I/O models to choose from, namely
+%
+%   [1] Inoperability Input-Output Model (IIM), cf.
+%       Christopher W. Anderson , Joost R. Santos & Yacov Y. Haimes (2007) 
+%       A Risk- based Input-Output Methodology for Measuring the Effects of the August 2003 Northeast Blackout, 
+%       Economic Systems Research, 19:2, 183-204, DOI: 10.1080/09535310701330233
+%
+%   [2] Environmentally Extended Input-Output Analysis (EEIOA), cf.
+%       Justin Kitzes (2013)
+%       An Introduction to Environmentally-Extended Input-Output Analysis,
+%       Resources 2013, 2, 489-503; doi:10.3390/resources2040489
 %
 %   NOTE: see PARAMETERS in code
 %
@@ -28,13 +39,21 @@ function [total_subsector_risk, total_country_risk, indirect_subsector_risk, ind
 %       mriot structure whose basic properties are the same regardless of the
 %       provided mriot it is based on, see mrio_read_table;
 % OPTIONAL INPUT PARAMETERS:
-%   params: a struct containing several fields, one of them specifying what
-%       I-O approach is applied in this procedure to estimate indirect risk
+%   parameters: a structure to pass on parameters, with fields as
+%       (run params = mrio_get_params to obtain all default values)
+%       switch_io_approach: specifying what I-O approach is applied in this procedure 
+%           to estimate indirect risk, IIM (=1, default) or EEIOA (2)
 % OUTPUTS:
-%   subsector_risk: table with indirect risk per subsector/country combination 
+%   total_subsector_risk: table with indirect and direct risk per subsector/country combination 
 %       based on the risk measure chosen in one variable and three "label" variables 
 %       containing corresponding country names, country ISO codes and sector names.
-%   country_risk: table with indirect risk per country based on the risk measure chosen
+%   total_country_risk: table with indirect and direct risk per country based on the risk measure chosen
+%       in one variable and two "label" variables containing corresponding 
+%       country names and country ISO codes.
+%   indirect_subsector_risk: table with indirect risk per subsector/country combination 
+%       based on the risk measure chosen in one variable and three "label" variables 
+%       containing corresponding country names, country ISO codes and sector names.
+%   indirect_country_risk: table with indirect risk per country based on the risk measure chosen
 %       in one variable and two "label" variables containing corresponding 
 %       country names and country ISO codes.
 %   leontief_inverse: the leontief inverse matrix which relates final demand to production
@@ -42,6 +61,8 @@ function [total_subsector_risk, total_country_risk, indirect_subsector_risk, ind
 % MODIFICATION HISTORY:
 % Ediz Herms, ediz.herms@outlook.com, 20171207, initial
 % Kaspar Tobler, 20180119 implement returned results as tables to improve readability (countries and sectors corresponding to the values are visible on first sight).
+% Ediz Herms, ediz.herms@outlook.com, 20180411, option to choose between IIM and EEIOA methodology
+%
 
 indirect_subsector_risk = []; % init output
 indirect_country_risk = []; % init output
@@ -96,9 +117,6 @@ for column_i = 1:n_subsectors*n_mrio_countries
     end
 end % column_i
 
-% leontief inverse 
-leontief_inverse = inv(eye(size(climada_mriot.mrio_data)) - techn_coeffs);
-
 % direct intensity vector
 direct_intensity_vector = zeros(1,length(direct_subsector_risk)); % init
 for cell_i = 1:length(direct_subsector_risk)
@@ -110,38 +128,32 @@ end % cell_i
 % risk calculation
 switch params.switch_io_approach
     
-    case 1 % (Modified) Environmentally extended input-output analysis (EEIOA), cf.
-
-        % aggregate risk
-        % subsector_risk = (direct_intensity_vector * leontief_inverse) .* transpose(total_output);
-        % max risk
-        % subsector_risk = zeros(1, length(direct_subsector_risk));
-        % for column_i = 1:n_subsectors*n_mrio_countries
-        %     subsector_risk(column_i) = max(direct_intensity_vector .* leontief_inverse(:,column_i)') * total_output(column_i);
-        % end % column_i
-
-        % weighted average 
-        % total_input = nansum(climada_mriot.mrio_data, 1);
-        % subsector_risk = zeros(1, length(direct_subsector_risk));
-        % for column_i = 1:n_subsectors*n_mrio_countries
-        %     weights = zeros(length(direct_subsector_risk),1);
-        %     if ~isnan(climada_mriot.mrio_data(:,column_i)./total_input(column_i))
-        %         weights = climada_mriot.mrio_data(:,column_i)./total_input(column_i); % normalize with total output
-        %     end
-        %     subsector_risk(column_i) = ((direct_intensity_vector .* leontief_inverse(:,column_i)') * weights) * total_output(column_i);
-        % end % column_i
-
-        % mean
-        indirect_subsector_risk = zeros(1, length(direct_subsector_risk));
-        for column_i = 1:n_subsectors*n_mrio_countries
-            indirect_subsector_risk(column_i) = mean(direct_intensity_vector .* leontief_inverse(:,column_i)') * total_output(column_i); % + direct_subsector_risk(column_i);
-        end % column_i
-
-    case 2 % Inoperability Input-Output Model (IIM), cf. Anderson et al., 2007 [2]
+    case 1 % Inoperability Input-Output Model (IIM), cf. Anderson et al., 2007 [1]
         
-        indirect_subsector_risk = solve( inv(diag(x))*(direct_intensity_vector.*x) + inv(diag(x))*leontief_inverse*(direct_intensity_vector.*x)+inv(diag(x))* total_output, x);
+        inv_total_output = 1./total_output;
+        inv_total_output(inv_total_output==Inf) = 0;
         
-    otherwise
+        % inverse of diagonalized production vector = \hat(x)^{-1}
+        inv_diag_total_output = diag(inv_total_output);
+        
+        % leontief inverse = (I-A^*)^{-1}
+        leontief_inverse = inv(eye(size(climada_mriot.mrio_data)) - inv_diag_total_output * techn_coeffs * diag(total_output));
+        
+        % normalized degraded final demand = (1-A^*)*q 
+        degr_final_demand = leontief_inverse * direct_intensity_vector';
+        
+        % denormalize 
+        indirect_subsector_risk = (degr_final_demand .* total_output)';
+        
+    case 2 % Environmentally Extended Input-Output Analysis (EEIOA), cf. Kitzes (2013) [2]
+        
+        % leontief inverse 
+        leontief_inverse = inv(eye(size(climada_mriot.mrio_data)) - techn_coeffs);
+        
+        % multiplying the monetary input-output relation by the industry-specific factor requirements
+        indirect_subsector_risk = ((direct_intensity_vector * leontief_inverse) .* total_output)';
+    
+    otherwise % TO DO
         
 end % params.switch_io_approach
 
@@ -168,6 +180,5 @@ indirect_subsector_risk = table(climada_mriot.countries',climada_mriot.countries
                                 'VariableNames',{'Country','CountryISO','Subsector','IndirectSubsectorRisk'});
 indirect_country_risk = table(unique(climada_mriot.countries','stable'),unique(climada_mriot.countries_iso','stable'),indirect_country_risk',...
                                 'VariableNames',{'Country','CountryISO','IndirectCountryRisk'});
-
 
 end % mrio leontief calc
