70b7914c185db0b6a3ab51d45f9f9c9eb272400a
Unknown <samweli@posteo.de>
2018-05-07 14:23:00 +0200

    20180507, tested for octave, scaling down damagefunctions for JPN, TWN, finalized questions and settings in climada_tc_play_go

--- code/climada_tc_play.m
+++ code/climada_tc_play.m
@@ -5,36 +5,47 @@
 % NAME:
 %   climada_tc_play
 % PURPOSE:
-%   Playful approach to TC risk in some countries
 %
-%   Q1.0: how to distribute the country_cover (total 5e9) in order to maximize the
-%   return (you get 30% of the expected damage as profit)
-%   country_attach=[];country_premium =[];prob_switch=0; 
-%   --> compare return on equity (RoE_sum)
-%   group with largest RoE wins
-%   => learning: put all to the max exposed country
+%  Usually called from climada_tc_play_go
 %
-%   Q1.1: same, with probabilistic events, prob_switch=1
-%   --> compare return on equity (RoE_sum)
-%   group with largest RoE wins
-%   => learning: probabilistic fills gaps, usually lowers RoE a bit
-%
-%   Q2.0: in addition, how to distribute a retention (attachement) of 5e9 in
-%   order to maximize the profit
-%   => lowers RoE, makes not too much sense if only profit maximised
-%   Q2.1: same, but probabilistic
-%
-%   Q3.0: if you would like to avoid paying out more than 2 bn in any year,
-%   how would you distribute covers? (attach of 1 bn per country)
-%   Q3.1: same, but probabilistic
-%   => need to diversify globally, at least in three countries
-%
-%   Q4.0: if you would like to reduce the payout frequency to once in five years, 
-%   how would you choose attachement and cover per country?
-%   Find a good trade-off between high RoE, low max. payout for payout period >= 5.
-%   Q4.1: same, but probabilistic
-%   => attachement matters to increase payout period and optimize your
-%   risks
+% 
+% Play around with TC damage for different countries, compare the effect of attachment point (retention) and diversification for an insurance point-of-view. 
+% 
+%  ~~~ Questions (to be implemented in climada_tc_play_go) ~~~
+% 
+% Q1.0 How to distribute the cover to the five countries (total cover USD 5 bn)
+% in order to maximize the return on equity (you get 30% of the expected damage as profit).
+% Note: Cover vector is normalized to sum up to USD 5 bn (5e9). No attachment point is set in this question.
+% Set: country_attach=[];country_premium =[];prob_switch=0; 
+% --> compare return on equity (RoE_sum)
+% group with largest RoE wins
+% => learning: put all to the max exposed country
+% 
+% Q1.1: same, with probabilistic events, prob_switch=1
+% --> compare return on equity (RoE_sum)
+% group with largest RoE wins
+% => learning: probabilistic fills gaps, usually lowers RoE a bit
+% 
+% Q2.0: In addition, how to distribute a retention (attachment) of USD 1 bn (1e9) in
+% order to maximize the profit?
+% => learning: lowers RoE, makes not too much sense if only profit maximized
+% 
+% Q2.1: same, but probabilistic
+% 
+% Q3.0: If you would like to avoid paying out more than 2 bn USD in any year,
+% how would you distribute covers over the 5 countirs? Use a fixed retention of USD 0.2 bn per country
+% => learning: need to diversify globally, at least in three countries
+% 
+% Q3.1: same, but probabilistic
+% 
+% Q4.0: If you would like to reduce the payout frequency to once in ten years, 
+% how would you choose retention and cover per country? Set attachment and cover freely.
+% Find a good trade-off between high RoE, low max. payout for payout period >= 10.
+% => learning: attachment matters to increase payout period and optimize your risks
+% 
+% Q4.1: same, but probabilistic
+% 
+
 %   
 %   previous call: none
 %   next call: many
@@ -50,6 +61,7 @@
 % David N. Bresch, david.bresch@gmail.com, 20180503 init
 % Samuel Eberenz, eberenz@posteo.eu, 20180503, add payout_period + Q4
 % David N. Bresch, david.bresch@gmail.com, 20180505, octave compatibility (half way)
+% Samuel Eberenz, eberenz@posteo.eu, 20180507, tested for octave, scaling down damagefunctions for JPN, TWN, finalized questions and settings in climada_tc_play_go
 %-
 
 RoE_sum=[]; % init output
@@ -80,8 +92,10 @@ if climada_global.octave_mode
     % special preparation for Octave
     if ~exist('hazard_hist','var')
         hazard_hist = climada_hazard_load('GLB_0360as_TC_hist_OCT');
+        hazard_hist = climada_hazard_reset_yearset(hazard_hist,1,1);
         hazard_prob=[];
     end
+    
     prob_switch=0; % no probabilistic for Octave (takes too long)
     
 else
@@ -118,15 +132,17 @@ if ~exist('YDS_hist','var') %to force: clear YDS_hist YDS_prob
         
         if ~climada_global.octave_mode, save(entity_file,'entity','-v6'); end % save as -v6 for later use in Octave
 
-        if isequal(country_ISO3,'JPN'),entity.damagefunctions.MDD=entity.damagefunctions.MDD*0.05;end
-        if isequal(country_ISO3,'TWN'),entity.damagefunctions.MDD=entity.damagefunctions.MDD*0.05;end   
+        if isequal(country_ISO3,'JPN'),entity.damagefunctions.MDD=entity.damagefunctions.MDD*0.1;end
+        if isequal(country_ISO3,'TWN'),entity.damagefunctions.MDD=entity.damagefunctions.MDD*0.1;end   
         
         EDS_hist(country_i)=climada_EDS_calc(entity,hazard_hist);
         YDS_hist(country_i)=climada_EDS2YDS(EDS_hist(country_i),hazard_hist,[],[],1);
-
+        YDS_hist(country_i).annotation_name = country_names{country_i};
+        
         if ~climada_global.octave_mode
             EDS_prob(country_i)=climada_EDS_calc(entity,hazard_prob); % prob calc only in MATLAB
             YDS_prob(country_i)=climada_EDS2YDS(EDS_prob(country_i),hazard_prob,[],[],1);
+            YDS_prob(country_i).annotation_name = country_names{country_i};
         end
                 
     end % country_i
@@ -136,10 +152,12 @@ end % ~exist('EDS_hist','var')
 YDS=YDS_hist; % default is historic perspective
 if prob_switch==1,YDS=YDS_prob;end
 
+
+
 if isempty(country_attach),country_attach = zeros(1,n_countries)*1e9;end
 if isempty(country_cover),country_cover   = ones(1,n_countries)*1e9;end
 
-% normalize cover and attachement
+% normalize cover and attachment (no normalization if total_... = 0)
 if total_cover>0, country_cover  = country_cover./sum(country_cover)  *total_cover;end
 if total_attach>0 && sum(country_attach)>0,country_attach = country_attach./sum(country_attach)*total_attach;end
 
@@ -160,8 +178,11 @@ max_annual_payout=max(country_damage_set);
 threshold_index=floor(length(country_damage_set)/10);
 country_damage_set=sort(country_damage_set);
 tol_annual_damage=country_damage_set(end-threshold_index);
-payout_period = floor(length(find(country_damage_set==0))/(length(find(country_damage_set>0)))+1);
-
+if length(find(country_damage_set>0))>0
+    payout_period = floor(length(find(country_damage_set==0))/(length(find(country_damage_set>0)))+1);
+else
+    payout_period=Inf;
+end
 
 if isempty(country_premium),country_premium=country_damage*1.3;end
 
--- code/climada_tc_play_go.m
+++ code/climada_tc_play_go.m
@@ -1,200 +1,247 @@
 % climada_tc_play_go
-% just call in command window to call climada_tc_play, see all info there
-
+% just call in command window to call climada_tc_play, see all further info there
+%
+%
+%
 % country_names={'Japan','Taiwan','Australia','Jamaica','Barbados'};
 %                 1       2        3           4         5  
+%
+%  CALLING SEQUENCE
+%   This script calls climada_tc_play several times. hazard and entities
+%   are defined there
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-clear entity* hazard* total_* EDS* YDS* country* prob* payou* threshold* tol* max* n_countr* silent RoE*
-% set maximum cover and attachement globally
+
+% clear entity* hazard* total_* EDS* YDS* country* prob* payou* threshold* tol* max* n_countr* silent RoE*
+% set maximum cover and attachment globally
+
+plot_switch = 0; % Show plots of damage per return period and country
 total_cover  = 5e9;
 total_attach = 1e9;
 
+fprintf('\n \n ~~~ climada TC play ~~~\n \n');
+
+if climada_global.octave_mode
+    warning('No probabilistic hazard set for Octave (takes too long)');
+end
 % Q1
 % --
 %% group 1
-country_cover =[10 0 0 0 0]*5e8;
-country_attach=[0 0 0 0 0]*5e8;
+country_cover =[10 0 0 0 0]*5e8; % distribute a total of 10 points (total= USD 5bn)
 
-country_premium=[];
-prob_switch=0;silent=1;climada_tc_play
-fprintf('--> Q1.0 Group 1 (hist): RoE = %+2.3f\n',RoE_sum*100);
-prob_switch=1;silent=1;climada_tc_play
-fprintf('--> Q1.1 Group 1 (prob): RoE = %+2.3f\n',RoE_sum*100);
+    country_attach=[0 0 0 0 0]*5e8; % leaf at zero for Q1 (no retention)
+    country_premium=[];
+    prob_switch=0;silent=1;climada_tc_play
+    fprintf('\n--> Q1.0 Group 1 (hist): RoE = %+2.3f\n',RoE_sum*100);
+    if ~climada_global.octave_mode
+    prob_switch=1;silent=1;climada_tc_play
+    fprintf('--> Q1.1 Group 1 (prob): RoE = %+2.3f\n',RoE_sum*100);
+    end
 
 % group 2
 country_cover =[0 0 4 4 2]*5e8;
-country_attach=[0 0 0 0 0]*5e8;
 
-country_premium=[];
-prob_switch=0;silent=1;climada_tc_play
-fprintf('--> Q1.0 Group 2 (hist): RoE = %+2.3f\n',RoE_sum*100);
-prob_switch=1;silent=1;climada_tc_play
-fprintf('--> Q1.1 Group 2 (prob): RoE = %+2.3f\n',RoE_sum*100);
-%%
+    country_attach=[0 0 0 0 0]*5e8;
+    country_premium=[];
+    prob_switch=0;silent=1;climada_tc_play
+    fprintf('--> Q1.0 Group 2 (hist): RoE = %+2.3f\n',RoE_sum*100);
+    if ~climada_global.octave_mode
+    prob_switch=1;silent=1;climada_tc_play
+    fprintf('--> Q1.1 Group 2 (prob): RoE = %+2.3f\n',RoE_sum*100);
+    end
+    
 % group 3
 country_cover =[0 0 0 0 10]*5e8;
-country_attach=[0 0 0 0 0]*5e8;
 
-country_premium=[];
-prob_switch=0;silent=1;climada_tc_play
-fprintf('--> Q1.0 Group 3 (hist): RoE = %+2.3f\n',RoE_sum*100);
-prob_switch=1;silent=1;climada_tc_play
-fprintf('--> Q1.1 Group 3 (prob): RoE = %+2.3f\n',RoE_sum*100);
+    country_attach=[0 0 0 0 0]*5e8;
+    country_premium=[];
+    prob_switch=0;silent=1;climada_tc_play
+    fprintf('--> Q1.0 Group 3 (hist): RoE = %+2.3f\n',RoE_sum*100);
+    if ~climada_global.octave_mode
+    prob_switch=1;silent=1;climada_tc_play
+    fprintf('--> Q1.1 Group 3 (prob): RoE = %+2.3f\n',RoE_sum*100);
+    end
 %%
 % group 4
 country_cover =[2 2 2 2 2]*5e8;
-country_attach=[0 0 0 0 0]*5e8;
 
-country_premium=[];
-prob_switch=0;silent=1;climada_tc_play
-fprintf('--> Q1.0 Group 4 (hist): RoE = %+2.3f\n',RoE_sum*100);
-prob_switch=1;silent=1;climada_tc_play
-fprintf('--> Q1.1 Group 4 (prob): RoE = %+2.3f\n',RoE_sum*100);
-
-fprintf('\n')
+    country_attach=[0 0 0 0 0]*5e8;
+    country_premium=[];
+    prob_switch=0;silent=1;climada_tc_play
+    fprintf('--> Q1.0 Group 4 (hist): RoE = %+2.3f\n',RoE_sum*100);
+    if ~climada_global.octave_mode
+    prob_switch=1;silent=1;climada_tc_play
+    fprintf('--> Q1.1 Group 4 (prob): RoE = %+2.3f\n',RoE_sum*100);
+    end
+    fprintf('\n')
 %%
 % Q2
 % --
 % group 1
-country_cover =[10 0 0 0 0]*5e8;
-country_attach=[10 0 0 0 0]*5e8;
+country_cover =[10 0 0 0 0]*5e8; % distribute a total of 10 points (total= USD 5bn)
+country_attach=[10 0 0 0 0]*1e8; % distribute a total of 10 points (total= USD 1bn)
 
-country_premium=[];
-prob_switch=0;silent=1;climada_tc_play
-fprintf('--> Q2.0 Group 1 (hist): RoE = %+2.3f\n',RoE_sum*100);
-prob_switch=1;silent=1;climada_tc_play
-fprintf('--> Q2.1 Group 1 (prob): RoE = %+2.3f\n',RoE_sum*100);
+    country_premium=[];
+    prob_switch=0;silent=1;climada_tc_play
+    fprintf('--> Q2.0 Group 1 (hist): RoE = %+2.3f\n',RoE_sum*100);
+    if ~climada_global.octave_mode
+    prob_switch=1;silent=1;climada_tc_play
+    fprintf('--> Q2.1 Group 1 (prob): RoE = %+2.3f\n',RoE_sum*100);
+    end
 
 % group 2
 country_cover =[0 0 4 4 2]*5e8;
-country_attach=[2 2 2 2 2]*5e8;
+country_attach=[0 0 5 4 1]*1e8;
 
-country_premium=[];
-prob_switch=0;silent=1;climada_tc_play
-fprintf('--> Q2.0 Group 2 (hist): RoE = %+2.3f\n',RoE_sum*100);
-prob_switch=1;silent=1;climada_tc_play
-fprintf('--> Q2.1 Group 2 (prob): RoE = %+2.3f\n',RoE_sum*100);
-
+    country_premium=[];
+    prob_switch=0;silent=1;climada_tc_play
+    fprintf('--> Q2.0 Group 2 (hist): RoE = %+2.3f\n',RoE_sum*100);
+    if ~climada_global.octave_mode
+    prob_switch=1;silent=1;climada_tc_play
+    fprintf('--> Q2.1 Group 2 (prob): RoE = %+2.3f\n',RoE_sum*100);
+    end
 % group 3
 country_cover =[0 0 0 0 10]*5e8;
-country_attach=[0 0 0 0 10]*5e8;
+country_attach=[0 0 0 0 10]*1e8;
 
-country_premium=[];
-prob_switch=0;silent=1;climada_tc_play
-fprintf('--> Q2.0 Group 3 (hist): RoE = %+2.3f\n',RoE_sum*100);
-prob_switch=1;silent=1;climada_tc_play
-fprintf('--> Q2.1 Group 3 (prob): RoE = %+2.3f\n',RoE_sum*100);
+    country_premium=[];
+    prob_switch=0;silent=1;climada_tc_play
+    fprintf('--> Q2.0 Group 3 (hist): RoE = %+2.3f\n',RoE_sum*100);
+    if ~climada_global.octave_mode
+    prob_switch=1;silent=1;climada_tc_play
+    fprintf('--> Q2.1 Group 3 (prob): RoE = %+2.3f\n',RoE_sum*100);
+    end
 
 % group 4
 country_cover =[2 2 2 2 2]*5e8;
-country_attach=[2 2 2 2 2]*5e8;
+country_attach=[2 2 2 2 2]*1e8;
 
-country_premium=[];
-prob_switch=0;silent=1;climada_tc_play
-fprintf('--> Q2.0 Group 4 (hist): RoE = %+2.3f\n',RoE_sum*100);
-prob_switch=1;silent=1;climada_tc_play
-fprintf('--> Q2.1 Group 4 (prob): RoE = %+2.3f\n',RoE_sum*100);
+    country_premium=[];
+    prob_switch=0;silent=1;climada_tc_play
+    fprintf('--> Q2.0 Group 4 (hist): RoE = %+2.3f\n',RoE_sum*100);
+    if ~climada_global.octave_mode
+    prob_switch=1;silent=1;climada_tc_play
+    fprintf('--> Q2.1 Group 4 (prob): RoE = %+2.3f\n',RoE_sum*100);
+    end
+    fprintf('\n')
 
 %% Q3
 % --
-fprintf('\n')
 
 % group 1
-country_cover =[10 0 0 0 0]*5e8;
-country_attach=[2 2 2 2 2]*5e8;
+country_cover =[10 0 0 0 0]*5e8; % distribute a total of 10 points (total= USD 5bn)
 
-country_premium=[];
-prob_switch=0;silent=1;climada_tc_play
-fprintf('--> Q3.0 Group 1 (hist): RoE = %+2.3f, max annual payout %2.3g\n',RoE_sum*100,max_annual_payout);
-prob_switch=1;silent=1;climada_tc_play
-fprintf('--> Q3.1 Group 1 (prob): RoE = %+2.3f, max annual payout %2.3g\n',RoE_sum*100,max_annual_payout);
-
+    country_attach=[2 2 2 2 2]*1e8;  % do not change attachement point for Q3 (USD 0.2bn for each country)
+    country_premium=[];
+    prob_switch=0;silent=1;climada_tc_play
+    fprintf('--> Q3.0 Group 1 (hist): RoE = %+2.3f, max annual payout %2.3g\n',RoE_sum*100,max_annual_payout);
+    if ~climada_global.octave_mode
+    prob_switch=1;silent=1;climada_tc_play
+    fprintf('--> Q3.1 Group 1 (prob): RoE = %+2.3f, max annual payout %2.3g\n',RoE_sum*100,max_annual_payout);
+    end
 % group 2
 country_cover =[0 0 2 4 4]*5e8;
-country_attach=[2 2 2 2 2]*5e8;
 
-country_premium=[];
-prob_switch=0;silent=1;climada_tc_play
-fprintf('--> Q3.0 Group 2 (hist): RoE = %+2.3f, max annual payout %2.3g\n',RoE_sum*100,max_annual_payout);
-prob_switch=1;silent=1;climada_tc_play
-fprintf('--> Q3.1 Group 2 (prob): RoE = %+2.3f, max annual payout %2.3g\n',RoE_sum*100,max_annual_payout);
-
+    country_attach=[2 2 2 2 2]*1e8;
+    country_premium=[];
+    prob_switch=0;silent=1;climada_tc_play
+    fprintf('--> Q3.0 Group 2 (hist): RoE = %+2.3f, max annual payout %2.3g\n',RoE_sum*100,max_annual_payout);
+    if ~climada_global.octave_mode
+    prob_switch=1;silent=1;climada_tc_play
+    fprintf('--> Q3.1 Group 2 (prob): RoE = %+2.3f, max annual payout %2.3g\n',RoE_sum*100,max_annual_payout);
+    end
 % group 3
 country_cover =[2 2 2 2 2]*5e8;
-country_attach=[2 2 2 2 2]*5e8;
 
-country_premium=[];
-prob_switch=0;silent=1;climada_tc_play
-fprintf('--> Q3.0 Group 3 (hist): RoE = %+2.3f, max annual payout %2.3g\n',RoE_sum*100,max_annual_payout);
-prob_switch=1;silent=1;climada_tc_play
-fprintf('--> Q3.1 Group 3 (prob): RoE = %+2.3f, max annual payout %2.3g\n',RoE_sum*100,max_annual_payout);
-
+    country_attach=[2 2 2 2 2]*1e8;
+    country_premium=[];
+    prob_switch=0;silent=1;climada_tc_play
+    fprintf('--> Q3.0 Group 3 (hist): RoE = %+2.3f, max annual payout %2.3g\n',RoE_sum*100,max_annual_payout);
+    if ~climada_global.octave_mode
+    prob_switch=1;silent=1;climada_tc_play
+    fprintf('--> Q3.1 Group 3 (prob): RoE = %+2.3f, max annual payout %2.3g\n',RoE_sum*100,max_annual_payout);
+    end
 % group 4
-country_cover =[2 2 2 3 1]*5e8;
-country_attach=[3 2 2 2 1]*5e8;
+country_cover =[.5 .5 4 4 1]*5e8;
 
-country_premium=[];
-prob_switch=0;silent=1;climada_tc_play
-fprintf('--> Q3.0 Group 4 (hist): RoE = %+2.3f, max annual payout %2.3g\n',RoE_sum*100,max_annual_payout);
-prob_switch=1;silent=1;climada_tc_play
-fprintf('--> Q3.1 Group 4 (prob): RoE = %+2.3f, max annual payout %2.3g\n',RoE_sum*100,max_annual_payout);
-
-fprintf('\n')
+    country_attach=[2 2 2 2 2]*1e8;
+    country_premium=[];
+    prob_switch=0;silent=1;climada_tc_play
+    fprintf('--> Q3.0 Group 4 (hist): RoE = %+2.3f, max annual payout %2.3g\n',RoE_sum*100,max_annual_payout);
+    if ~climada_global.octave_mode
+    prob_switch=1;silent=1;climada_tc_play
+    fprintf('--> Q3.1 Group 4 (prob): RoE = %+2.3f, max annual payout %2.3g\n',RoE_sum*100,max_annual_payout);
+    end
+    fprintf('\n')
 
 %% Q4: Payout period and free adjustment of "attach"
 % --
 
-total_attach = 0; % <-- 0, so that attachement can be set freely
+total_attach = 0; % <-- if set to 0, attachment can be choosen freely, otherwise it is always normalized to the value of total_attach, i.e. 1e9 USD.
+
 % group 1
 country_cover =[10 0 0 0 0]*5e8;
-country_attach=[10 0 0 0 0]*5e8;
+country_attach=[5 0 0 0 0]*1e8;
 
-country_premium=[];
-prob_switch=0;silent=1;climada_tc_play
-fprintf('--> Q4.0 Group 1 (hist): RoE = %+2.3f, max annual payout %2.3g, payout every %i year(s)\n',RoE_sum*100,max_annual_payout,payout_period);
-prob_switch=1;silent=1;climada_tc_play
-fprintf('--> Q4.1 Group 1 (prob): RoE = %+2.3f, max annual payout %2.3g, payout every %i year(s)\n',RoE_sum*100,max_annual_payout,payout_period)
-
+    country_premium=[];
+    prob_switch=0;silent=1;climada_tc_play
+    fprintf('--> Q4.0 Group 1 (hist): RoE = %+2.3f, max annual payout %2.3g, payout every %i year(s)\n',RoE_sum*100,max_annual_payout,payout_period);  
+    if ~climada_global.octave_mode
+    prob_switch=1;silent=1;climada_tc_play
+    fprintf('--> Q4.1 Group 1 (prob): RoE = %+2.3f, max annual payout %2.3g, payout every %i year(s)\n',RoE_sum*100,max_annual_payout,payout_period)
+    end
 % group 2
 country_cover =[0 0 4 2 4]*5e8;
-country_attach=[0 0 4 2 4]*5e8;
+country_attach=[0 0 4 2 4]*1e8;
 
-country_premium=[];
-prob_switch=0;silent=1;climada_tc_play
-fprintf('--> Q4.0 Group 2 (hist): RoE = %+2.3f, max annual payout %2.3g, payout every %i year(s)\n',RoE_sum*100,max_annual_payout,payout_period);
-prob_switch=1;silent=1;climada_tc_play
-fprintf('--> Q4.1 Group 2 (prob): RoE = %+2.3f, max annual payout %2.3g, payout every %i year(s)\n',RoE_sum*100,max_annual_payout,payout_period)
-
+    country_premium=[];
+    prob_switch=0;silent=1;climada_tc_play
+    fprintf('--> Q4.0 Group 2 (hist): RoE = %+2.3f, max annual payout %2.3g, payout every %i year(s)\n',RoE_sum*100,max_annual_payout,payout_period);
+    if ~climada_global.octave_mode
+    prob_switch=1;silent=1;climada_tc_play
+    fprintf('--> Q4.1 Group 2 (prob): RoE = %+2.3f, max annual payout %2.3g, payout every %i year(s)\n',RoE_sum*100,max_annual_payout,payout_period)
+    end
 % group 3
 country_cover =[2 2 2 2 2]*5e8;
-country_attach=[5 5 5 5 5]*5e8;
+country_attach=[5 5 5 5 5]*1e8;
 
-country_premium=[];
-prob_switch=0;silent=1;climada_tc_play
-fprintf('--> Q4.0 Group 3 (hist): RoE = %+2.3f, max annual payout %2.3g, payout every %i year(s)\n',RoE_sum*100,max_annual_payout,payout_period);
-prob_switch=1;silent=1;climada_tc_play
-fprintf('--> Q4.1 Group 3 (prob): RoE = %+2.3f, max annual payout %2.3g, payout every %i year(s)\n',RoE_sum*100,max_annual_payout,payout_period)
-
+    country_premium=[];
+    prob_switch=0;silent=1;climada_tc_play
+    fprintf('--> Q4.0 Group 3 (hist): RoE = %+2.3f, max annual payout %2.3g, payout every %i year(s)\n',RoE_sum*100,max_annual_payout,payout_period);
+    if ~climada_global.octave_mode
+    prob_switch=1;silent=1;climada_tc_play
+    fprintf('--> Q4.1 Group 3 (prob): RoE = %+2.3f, max annual payout %2.3g, payout every %i year(s)\n',RoE_sum*100,max_annual_payout,payout_period)
+    end
 % group 4
 country_cover =[2 2 2 2 2]*5e8;
-country_attach=[1.5 1.8 2 3 3]*5e8;
+country_attach=[1.5 1.8 2 3 3]*1e8;
 
-country_premium=[];
-prob_switch=0;silent=1;climada_tc_play
-fprintf('--> Q4.0 Group 4 (hist): RoE = %+2.3f, max annual payout %2.3g, payout every %i year(s)\n',RoE_sum*100,max_annual_payout,payout_period);
-prob_switch=1;silent=1;climada_tc_play
-fprintf('--> Q4.1 Group 4 (prob): RoE = %+2.3f, max annual payout %2.3g, payout every %i year(s)\n',RoE_sum*100,max_annual_payout,payout_period)
-
+    country_premium=[];
+    prob_switch=0;silent=1;climada_tc_play
+    fprintf('--> Q4.0 Group 4 (hist): RoE = %+2.3f, max annual payout %2.3g, payout every %i year(s)\n',RoE_sum*100,max_annual_payout,payout_period);
+    if ~climada_global.octave_mode
+    prob_switch=1;silent=1;climada_tc_play
+    fprintf('--> Q4.1 Group 4 (prob): RoE = %+2.3f, max annual payout %2.3g, payout every %i year(s)\n',RoE_sum*100,max_annual_payout,payout_period)
+    end
 %%
 
 % plot
-close all
-figure; climada_EDS_DFC(YDS(1:end));
-xlim([0 100]),ylim([0 1e9])
+if plot_switch
+    close all
+    figure('InnerPosition',[10 10 700 450]);
+    climada_EDS_DFC(YDS(1:end));
+    xlim([0 100]),ylim([0 1e9])
+    legend('Location','NorthEast')
+    title('Damage exceedence Frequency Curve (DFC)');
 
-figure; climada_EDS_DFC(YDS(1:2));
-xlim([0 100]),ylim([0 5e9])
+    figure('InnerPosition',[10 10 700 450]); 
+    climada_EDS_DFC(YDS(1:2));
+    xlim([0 100]),ylim([0 5e9])
+    title('Damage exceedence Frequency Curve (DFC)');
 
-figure; climada_EDS_DFC(YDS(3:end));
-xlim([0 100]),ylim([0 5e8])
+    figure('InnerPosition',[10 10 700 450]);
+    climada_EDS_DFC(YDS(3:end));
+    xlim([0 100]),ylim([0 5e8])
+    title('Damage exceedence Frequency Curve (DFC)');
+end
 
